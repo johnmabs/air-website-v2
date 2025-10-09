8b4aeb6927395512bf5e4cfa384509ca5f2b9af4
John Mabiala <johnmabiala@hotmail.com>
2025-10-09 08:50:20 +0100

    Improved navbar for mobile devices

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,50 @@
-console.log("Hello, World!");
+const btnOpen = document.querySelector("#btnOpen");
+const btnClose = document.querySelector("#btnClose");
+const media = window.matchMedia("(width < 56.25em)");
+const primaryNavigation = document.querySelector(".primary-navigation");
+const main = document.querySelector("main");
+const body = document.querySelector("body");
+
+function setupPrincipalNav(e) {
+  if (e.matches) {
+    // is mobile
+    console.log("is mobile");
+    primaryNavigation.setAttribute("inert", "");
+    primaryNavigation.style.transition = "none";
+  } else {
+    // is  tablet / desktop
+    console.log("is desktop");
+    closeMobileMenu();
+    primaryNavigation.removeAttribute("inert");
+  }
+}
+
+function openMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "true");
+  primaryNavigation.removeAttribute("inert");
+  primaryNavigation.removeAttribute("style");
+  main.setAttribute("inert", "");
+  bodyScrollLockUpgrade.disableBodyScroll(body);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute("aria-expanded", "false");
+  primaryNavigation.setAttribute("inert", "");
+  main.removeAttribute("inert");
+  bodyScrollLockUpgrade.enableBodyScroll(body);
+  btnOpen.focus();
+
+  setTimeout(() => {
+    primaryNavigation.style.transition = "none";
+  }, 500);
+}
+
+setupPrincipalNav(media);
+
+btnOpen.addEventListener("click", openMobileMenu);
+btnClose.addEventListener("click", closeMobileMenu);
+
+media.addEventListener("change", function (e) {
+  setupPrincipalNav(e);
+});
